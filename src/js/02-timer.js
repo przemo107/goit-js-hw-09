bd4f404ae81fc0d1a814fc79e6775136ca37462d
Przemysław Soliński <solinskip107@gmail.com>
2023-04-26 21:44:19 +0200

    timer not working yet

---
+++ src/js/02-timer.js
@@ -0,0 +1,58 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+    if (selectedDate.getTime() < currentDate.getTime()) {
+      window.alert('Please choose a date in the future');
+      document.querySelector('[data-start]').disabled = true;
+    } else {
+      document.querySelector('[data-start]').disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
+const startButton = document.querySelector('[data-start]');
+
+let intervalId;
+
+function startTimer() {
+  const selectedDate = new Date(
+    document.querySelector('#datetime-picker').value
+  );
+  const currentDate = new Date();
+  const differenceMs = selectedDate - currentDate;
+
+  if (differenceMs <= 0) {
+    clearInterval(intervalId);
+    daysValue.innerText = '00';
+    hoursValue.innerText = '00';
+    minutesValue.innerText = '00';
+    secondsValue.innerText = '00';
+    startButton.disabled = true;
+    return;
+  }
+
+  const { days, hours, minutes, seconds } = convertMs(differenceMs);
+
+  daysValue.innerText = days.toString().padStart(2, '0');
+  hoursValue.innerText = hours.toString().padStart(2, '0');
+  minutesValue.innerText = minutes.toString().padStart(2, '0');
+  secondsValue.innerText = seconds.toString().padStart(2, '0');
+}
+
+startButton.addEventListener('click', () => {
+  intervalId;
+});
