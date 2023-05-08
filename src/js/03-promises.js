b19965260aa5120c0f8bf268df5d7c6c3ce7c40f
Przemysław Soliński <solinskip107@gmail.com>
2023-05-08 20:22:42 +0200

    promises done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,14 +12,10 @@ form.addEventListener('submit', e => {
     const promiseDelay = delay + (i - 1) * step;
     createPromise(position, promiseDelay)
       .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
-        );
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
-        );
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       });
   }
 });
